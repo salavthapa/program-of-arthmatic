d32e6d4d6680436ca2f9c02c72c0009775480e6f
salavthapa <salav.thapa4@gmail.com>
2016-11-25 11:00:37 +0545

    Add files via upload

---
+++ check number is odd or even.c
@@ -0,0 +1,32 @@
+/*
+Student Name:Salav Thapa
+Subject:Programing Fundamental
+Roll:
+program:check the number is odd or even
+Lab Sheet No:6
+Date:2016-11-25
+*/
+
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter the number:\n");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("the number is even \n");
+     }
+    else
+    {
+        printf("the number is odd \n");
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
