faf3d20a89c4f3abfa4a7641f8140cc44fc10e06
salavthapa <salav.thapa4@gmail.com>
2016-11-25 10:43:13 +0545

    Add files via upload

---
+++ check number positive or negative.c
@@ -0,0 +1,22 @@
+/*
+Student Name:Salav Thapa
+Subject:Programing Fundamental
+Roll:
+program:check the number is positive or negative
+Lab Sheet No:6
+Date:2016-11-25
+*/
+
+#include<stdio.h>
+int main(){
+    int a;
+    printf("E                                                                                                     nter the number:\n");
+    scanf("%d",&a);
+    if("n>0");
+    {
+        printf("give number is positive");
+    }
+
+    return 0;
+
+}
