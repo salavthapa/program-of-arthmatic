d8d3fda7051f5e856ee979a03dbae75925f0e60c
salavthapa <salav.thapa4@gmail.com>
2016-11-25 10:04:26 +0545

    Add files via upload

---
+++ program of arthmatic.c
@@ -0,0 +1,35 @@
+/*
+Student Name:Salav Thapa
+Subject:Programing Fundamental
+Roll:
+Lab Sheet No:6
+Date:2016-11-25
+*/
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a,b,sum,sub,multi,divi,modulus;
+    printf("enter the first number:\n");
+    scanf("%d",&a);
+    printf("enter the second number:\n");
+    scanf("%d",&b);
+    sum=(a+b);
+    sub=(a-b);
+    multi=(a*b);
+    divi=(a/b);
+    modulus=(a%b);
+    printf("the sum is %d \n",sum);
+    printf("the sub is %d \n",sub);
+    printf("the multi is %d \n",multi);
+    printf("the divi is %d \n",divi);
+    printf("the modulus is %d \n",modulus);
+    return 0;
+}
+
+
+
+
+
+
+
